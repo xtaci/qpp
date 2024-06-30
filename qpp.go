3c2b651668ad80c0c614b5a08b35ac3805317f46
xtaci <daniel820313@gmail.com>
2024-06-30 20:49:03 +0800

    initial implementation on 8-qubit

--- qpp.go
+++ qpp.go
@@ -1,34 +1,83 @@
 package qpp
 
 import (
+	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/binary"
+	"fmt"
+	"math/rand"
 )
+
+const PAD_IDENTIFIER = "QPP_%b"
+const PM_SELECTOR_IDENTIFIER = "PERMUTATION_MATRIX_SELECTOR"
+const NATIVE_BYTE_LENGTH = 8 // bit
 
 type QuantumPermutationPad struct {
 	pads  [][]byte // encryption
 	rpads [][]byte // decryption
 
-	numPads int // number of pads
+	numPads      uint16     // number of pads
+	qubits       uint8      // number of quantum bits
+	bytesEncoded uint64     // number of bytes encoded
+	bytesDecoded uint64     // number of bytes decoded
+	encRand      *rand.Rand // random source for pattern selection
+	decRand      *rand.Rand // random source for pattern selection
 }
 
-func NewQPP(seed []byte, numPads int) *QuantumPermutationPad {
+func NewQPP(seed []byte, numPads uint16, qubits uint8) *QuantumPermutationPad {
 	qpp := &QuantumPermutationPad{
 		numPads: numPads,
+		qubits:  qubits,
 	}
 
 	qpp.pads = make([][]byte, numPads)
 	qpp.rpads = make([][]byte, numPads)
 
-	for i := 0; i < numPads; i++ {
-		qpp.pads[i] = make([]byte, 256)
-		qpp.rpads[i] = make([]byte, 256)
+	for i := uint16(0); i < numPads; i++ {
+		qpp.pads[i] = make([]byte, 1<<qubits)
+		qpp.rpads[i] = make([]byte, 1<<qubits)
 		fill(qpp.pads[i])
-		shuffle(seed, qpp.pads[i])
+		shuffle(seed, qpp.pads[i], i)
 		reverse(qpp.pads[i], qpp.rpads[i])
 	}
 
+	mac := hmac.New(sha256.New, seed)
+	mac.Write([]byte(PM_SELECTOR_IDENTIFIER))
+	sum := mac.Sum(nil)
+
+	encSource := rand.NewSource(int64(binary.LittleEndian.Uint64(sum)))
+	qpp.encRand = rand.New(encSource)
+	decSource := rand.NewSource(int64(binary.LittleEndian.Uint64(sum)))
+	qpp.decRand = rand.New(decSource)
+
 	return qpp
+}
+
+// Encrypt encrypts data using the Quantum Permutation Pad
+func (qpp *QuantumPermutationPad) Encrypt(data []byte) {
+	switch qpp.qubits {
+	case NATIVE_BYTE_LENGTH:
+		for i := 0; i < len(data); i++ {
+			index := qpp.encRand.Uint32() % uint32(qpp.numPads)
+			pad := qpp.pads[index]
+			data[i] = pad[data[i]]
+		}
+	default:
+	}
+	qpp.bytesEncoded += uint64(len(data))
+}
+
+func (qpp *QuantumPermutationPad) Decrypt(data []byte) {
+	switch qpp.qubits {
+	case NATIVE_BYTE_LENGTH:
+		for i := 0; i < len(data); i++ {
+			index := qpp.decRand.Uint32() % uint32(qpp.numPads)
+			rpad := qpp.rpads[index]
+			data[i] = rpad[data[i]]
+		}
+	default:
+	}
+	qpp.bytesDecoded += uint64(len(data))
 }
 
 func fill(pad []byte) {
@@ -43,12 +92,14 @@ func reverse(pad []byte, rpad []byte) {
 	}
 }
 
-func shuffle(seed []byte, pad []byte) {
-	for i := len(pad) - 1; i > 0; i-- {
-		s := sha256.Sum256(seed)
-		seed = s[:]
+func shuffle(seed []byte, pad []byte, padID uint16) {
+	message := fmt.Sprintf(PAD_IDENTIFIER, padID)
+	mac := hmac.New(sha256.New, seed)
+	mac.Write([]byte(message))
+	sum := mac.Sum(nil)
+	source := rand.NewSource(int64(binary.LittleEndian.Uint64(sum)))
 
-		j := binary.LittleEndian.Uint64(seed) % uint64(i+1)
+	rand.New(source).Shuffle(len(pad), func(i, j int) {
 		pad[i], pad[j] = pad[j], pad[i]
-	}
+	})
 }
--- qpp_test.go
+++ qpp_test.go
@@ -8,10 +8,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestQPP(t *testing.T) {
+func TestPads(t *testing.T) {
 	seed := make([]byte, 32)
 	io.ReadFull(rand.Reader, seed)
-	qpp := NewQPP(seed, 4)
+	qpp := NewQPP(seed, 8, 8)
 	t.Log(qpp.pads[0])
 	t.Log(qpp.rpads[0])
 	assert.Equal(t, len(qpp.pads), len(qpp.rpads), "pads not equal")
@@ -19,5 +19,34 @@ func TestQPP(t *testing.T) {
 		for j := 0; j < len(qpp.pads[i]); j++ {
 			assert.Equal(t, qpp.rpads[i][qpp.pads[i][j]], byte(j), "not reservable")
 		}
+	}
+}
+
+func TestEncryption(t *testing.T) {
+	seed := make([]byte, 32)
+	io.ReadFull(rand.Reader, seed)
+
+	sender := NewQPP(seed, 8, 8)
+	receiver := NewQPP(seed, 8, 8)
+
+	original := []byte("hello world")
+	msg := []byte("hello world")
+	sender.Encrypt(msg)
+	assert.NotEqual(t, original, msg, "not encrypted")
+	receiver.Decrypt(msg)
+	t.Log(msg)
+	assert.Equal(t, original, msg, "not equal")
+}
+
+func BenchmarkEncryption(b *testing.B) {
+	seed := make([]byte, 32)
+	io.ReadFull(rand.Reader, seed)
+
+	sender := NewQPP(seed, 8, 8)
+	msg := []byte("hello world")
+	b.ResetTimer()
+	b.SetBytes(int64(len(msg)))
+	for i := 0; i < b.N; i++ {
+		sender.Encrypt(msg)
 	}
 }
